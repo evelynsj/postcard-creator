1fa51978c71889940dee5eaca40245e9376e6047
Evelyn Sjafii <evelynsj98@gmail.com>
2020-04-21 00:12:34 -0700

    Added color palette functionality

--- public/script.js
+++ public/script.js
@@ -1,3 +1,5 @@
+const ACTIVE = "active";
+
 // Change fonts
 
 const FONTS = {
@@ -23,7 +25,6 @@ const FONTS = {
     },
 };
 
-const ACTIVE = "active";
 let currentFontIdx = 0;
 let fontStyles = document.getElementById("font-styles");
 let listItems = fontStyles.getElementsByTagName("li");
@@ -55,3 +56,96 @@ let changeFont = (e) => {
 };
 
 fontStyles.addEventListener("click", changeFont);
+
+// Change color
+
+const COLORS = {
+    "color-1": {
+        index: 0,
+        color: "#e6e2cf",
+    },
+    "color-2": {
+        index: 1,
+        color: "#dbcaac",
+    },
+    "color-3": {
+        index: 2,
+        color: "#c9cbb3",
+    },
+    "color-4": {
+        index: 3,
+        color: "#bbc9ca",
+    },
+    "color-5": {
+        index: 4,
+        color: "#a6a5b5",
+    },
+    "color-6": {
+        index: 5,
+        color: "#b5a6ab",
+    },
+    "color-7": {
+        index: 6,
+        color: "#eccfcf",
+    },
+    "color-8": {
+        index: 7,
+        color: "#eceeeb",
+    },
+    "color-9": {
+        index: 8,
+        color: "#bab9b5",
+    },
+};
+
+let currentColorIdx = 0;
+let colorOptions = document.getElementById("color-options");
+let colorButtons = colorOptions.getElementsByTagName("button");
+let previousColor = COLORS["color-1"].color; // for hovering
+let postcard = document.getElementsByClassName("postcard-container")[0];
+
+let changeColor = (e) => {
+    if (e.target && e.target.nodeName === "BUTTON") {
+        if (e.target.classList.contains(ACTIVE)) {
+            return; // already active
+        } else {
+            // remove active class from previous color
+            colorButtons[currentColorIdx].classList.remove(ACTIVE);
+
+            // set currentColor index
+            let currentColor = e.target.classList[1];
+            currentColorIdx = COLORS[currentColor].index;
+
+            // set new color for postcard
+            postcard.style.background = COLORS[currentColor].ACTIVE;
+
+            // add active class to classList of chosen element
+            e.target.classList.add(ACTIVE);
+        }
+    }
+};
+
+let viewColor = (e) => {
+    if (e.target && e.target.nodeName === "BUTTON") {
+        let colorChosen = e.target.classList[1];
+
+        // store color to revert back to
+        for (let key in COLORS) {
+            if (COLORS[key]["index"] === currentColorIdx) {
+                previousColor = COLORS[key]["color"];
+            }
+        }
+
+        postcard.style.background = COLORS[colorChosen].color;
+    }
+};
+
+let revertColor = (e) => {
+    if (e.target && e.target.nodeName === "BUTTON") {
+        postcard.style.background = previousColor;
+    }
+};
+
+colorOptions.addEventListener("click", changeColor);
+colorOptions.addEventListener("mouseover", viewColor);
+colorOptions.addEventListener("mouseout", revertColor);
