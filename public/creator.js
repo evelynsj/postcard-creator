b258427884c84828da3c12b93ab93cc1f499ac2a
Evelyn Sjafii <evelynsj98@gmail.com>
2020-04-24 01:45:41 -0700

    Hover color

--- public/creator.js
+++ public/creator.js
@@ -184,6 +184,34 @@ const changeColor = (e) => {
     }
 };
 
+// PEEK COLOR
+
+const peekColor = (e) => {
+    if (e.target && e.target.nodeName === "BUTTON") {
+        if (e.target.classList[1] === postcard.background) {
+            return; // already active
+        } else {
+            const postcardContainer = document.getElementsByClassName(
+                "postcard-container"
+            )[0];
+            const selectedColor = e.target.classList[1];
+
+            postcardContainer.style.background = COLORS[selectedColor];
+        }
+    }
+};
+
+// REVERT COLOR
+
+const revertColor = (e) => {
+    if (e.target && e.target.nodeName === "BUTTON") {
+        const postcardContainer = document.getElementsByClassName(
+            "postcard-container"
+        )[0];
+        postcardContainer.style.background = COLORS[postcard.background];
+    }
+};
+
 const writeMessage = () => {
     const message = document.getElementById("write-message");
     postcard.text = message.textContent;
@@ -210,6 +238,13 @@ document.getElementById("font-styles").addEventListener("click", changeFont);
 document.getElementById("color-options").addEventListener("click", changeColor);
 
 document.getElementById("share-button").addEventListener("click", sendFile);
+
+document
+    .getElementById("color-options")
+    .addEventListener("mouseover", peekColor);
+document
+    .getElementById("color-options")
+    .addEventListener("mouseout", revertColor);
 
 /* DYNAMIC LOADING */
 
@@ -249,208 +284,3 @@ for (let color in COLORS) {
 /* CALL CONSTRUCTOR */
 
 let postcard = new Postcard();
-
-// const ACTIVE = "active";
-
-// // CHANGE FONT
-
-// const FONTS = {
-//     "indie-flower": {
-//         index: 0,
-//         name: "Indie Flower",
-//         size: "15px",
-//     },
-//     "dancing-script": {
-//         index: 1,
-//         name: "Dancing Script",
-//         size: "18px",
-//     },
-//     "long-cang": {
-//         index: 2,
-//         name: "Long Cang",
-//         size: "18px",
-//     },
-//     "homemade-apple": {
-//         index: 3,
-//         name: "Homemade Apple",
-//         size: "12px",
-//     },
-// };
-
-// let currentFontIdx = 0;
-// let fontStyles = document.getElementById("font-styles");
-// let listItems = fontStyles.getElementsByTagName("li");
-
-// let changeFont = (e) => {
-//     if (e.target && e.target.nodeName === "LI") {
-//         if (e.target.classList.contains(ACTIVE)) {
-//             return; // already active
-//         } else {
-//             // remove active class from previously chosen element
-//             listItems[currentFontIdx].classList.remove(ACTIVE);
-
-//             // set currentFont index
-//             let currentFont = e.target.classList[0];
-//             currentFontIdx = FONTS[currentFont].index;
-
-//             // set new font for write-message
-//             let writeMessage = document.getElementById("write-message");
-//             let font = FONTS[currentFont].name + ", sans-serif";
-//             writeMessage.style.fontFamily = font;
-//             writeMessage.style.fontSize = FONTS[currentFont].size;
-
-//             // add active to classList of chosen element
-//             e.target.classList.add(ACTIVE);
-//         }
-//     }
-// };
-
-// fontStyles.addEventListener("click", changeFont);
-
-// // CHANGE COLOR
-
-// const COLORS = {
-//     "color-1": {
-//         index: 0,
-//         color: "#e6e2cf",
-//     },
-//     "color-2": {
-//         index: 1,
-//         color: "#dbcaac",
-//     },
-//     "color-3": {
-//         index: 2,
-//         color: "#c9cbb3",
-//     },
-//     "color-4": {
-//         index: 3,
-//         color: "#bbc9ca",
-//     },
-//     "color-5": {
-//         index: 4,
-//         color: "#a6a5b5",
-//     },
-//     "color-6": {
-//         index: 5,
-//         color: "#b5a6ab",
-//     },
-//     "color-7": {
-//         index: 6,
-//         color: "#eccfcf",
-//     },
-//     "color-8": {
-//         index: 7,
-//         color: "#eceeeb",
-//     },
-//     "color-9": {
-//         index: 8,
-//         color: "#bab9b5",
-//     },
-// };
-
-// let currentColorIdx = 0;
-// let colorOptions = document.getElementById("color-options");
-// let colorButtons = colorOptions.getElementsByTagName("button");
-// let previousColor = COLORS["color-1"].color; // for hovering
-// let postcard = document.getElementsByClassName("postcard-container")[0];
-
-// let changeColor = (e) => {
-//     if (e.target && e.target.nodeName === "BUTTON") {
-//         if (e.target.classList.contains(ACTIVE)) {
-//             return; // already active
-//         } else {
-//             // remove active class from previous color
-//             colorButtons[currentColorIdx].classList.remove(ACTIVE);
-
-//             // set currentColor index
-//             let currentColor = e.target.classList[1];
-//             currentColorIdx = COLORS[currentColor].index;
-
-//             // set new color for postcard
-//             postcard.style.background = COLORS[currentColor].ACTIVE;
-
-//             // add active class to classList of chosen element
-//             e.target.classList.add(ACTIVE);
-//         }
-//     }
-// };
-
-// let viewColor = (e) => {
-//     if (e.target && e.target.nodeName === "BUTTON") {
-//         let colorChosen = e.target.classList[1];
-
-//         // store color to revert back to
-//         for (let key in COLORS) {
-//             if (COLORS[key]["index"] === currentColorIdx) {
-//                 previousColor = COLORS[key]["color"];
-//             }
-//         }
-
-//         postcard.style.background = COLORS[colorChosen].color;
-//     }
-// };
-
-// let revertColor = (e) => {
-//     if (e.target && e.target.nodeName === "BUTTON") {
-//         postcard.style.background = previousColor;
-//     }
-// };
-
-// colorOptions.addEventListener("click", changeColor);
-// colorOptions.addEventListener("mouseover", viewColor);
-// colorOptions.addEventListener("mouseout", revertColor);
-
-// // SEND POSTCARD
-
-// const getCurrentFont = () => {
-//     for (let font in FONTS) {
-//         if (FONTS[font]["index"] === currentFontIdx) {
-//             return FONTS[font]["name"];
-//         }
-//     }
-// };
-
-// const getCurrentBackground = () => {
-//     for (let color in COLORS) {
-//         if (COLORS[color]["index"] === currentColorIdx) {
-//             return COLORS[color]["color"];
-//         }
-//     }
-// };
-
-// const sendData = () => {
-//     console.log("Send Data");
-//     let writeMessage = document.getElementById("write-message");
-
-//     const data = {
-//         name: selectedImageName,
-//         message: writeMessage.textContent.trim(),
-//         font: `${getCurrentFont()}; sans-serif`,
-//         background: getCurrentBackground(),
-//     };
-
-//     const options = {
-//         method: "POST",
-//         headers: {
-//             "Content-Type": "application/json; charset=UTF-8",
-//         },
-//         body: JSON.stringify(data),
-//     };
-
-//     fetch("/share", options)
-//         .then((res) => {
-//             if (res.ok) {
-//                 return res.json();
-//             } else {
-//                 return Promise.reject(res);
-//             }
-//         })
-//         .then((data) => {
-//             console.log(data);
-//             // return fetch("/display").then(response => console.log(response))
-//             return (window.location = "display");
-//         });
-// };
-
-// let shareButton = document.getElementById("share-button");
-// shareButton.addEventListener("click", sendData);
